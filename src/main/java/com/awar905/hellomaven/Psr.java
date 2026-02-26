bc157ee010e045eeaefca8b8acd44825a0cff6a7
awar905 <awar905@aucklanduni.ac.nz>
2026-02-27 09:55:06 +1300

    Add: Javadoc Generation. Paper Scissors Rock API
    Add javadoc plugin to pom.xml
    Create Paper Scissors Rock class with general extendable functions
    Create Test for converting integer to Paper Scissors Rock Move

---
+++ src/main/java/com/awar905/hellomaven/Psr.java
@@ -0,0 +1,71 @@
+package com.awar905.hellomaven;
+
+import java.util.Optional;
+
+/**
+ * Paper Scissors Rock API
+ *
+ * Contains Base Types and Moves to implement.
+ * Should not be implemented than the `Psr` class
+ * @see com.awar905.hellomaven.Psr
+ */
+interface PsrAPI {
+  /**
+   * All possible moves in Paper Scissors Rock
+   */
+  public enum Move {
+    PAPER,
+    SCISSORS,
+    ROCK;
+  } 
+
+  /**
+   * Method to implement getting next move
+   */
+  public Move getMove();
+}
+
+public abstract class Psr implements PsrAPI {
+  public static Optional<Move> intToMove(int value) {
+    switch(value) {
+      case 0:
+        return Optional.of(Move.PAPER);
+      case 1:
+        return Optional.of(Move.SCISSORS);
+      case 2:
+        return Optional.of(Move.ROCK);
+      default:
+        // I'm using null because it is easy to detect and there is only one possible
+        // error that can occur. So creating a custom error is unecessary
+        return Optional.empty();
+    }
+  }
+
+  /**
+   * Determine the winner of the round given their respective moves
+   *
+   * @param player1 The move decided of the first player
+   * @param player2 The move decided by the second player
+   *
+   * @return 0 is returned when there is a tie.
+   *         1 is returned when player1 wins. 
+   *         2 is returned when player2 wins. 
+   *         No other result is a valid return value.
+   */
+  public static int getWinner(Move player1, Move player2) {
+    // Same move results in a tie
+    if(player1 == player2) return 0;
+
+    // Paper > Rock
+    // Scissors > Paper
+    // Rock > Scissors
+    switch(player1) {
+      case Move.PAPER:
+        return player2 == Move.ROCK ? 1 : 2;
+      case Move.SCISSORS:
+        return player2 == Move.PAPER ? 1 : 2;
+      case Move.ROCK:
+        return player2 == Move.SCISSORS ? 1 : 2;
+    }
+  }
+}
--- src/test/java/com/awar905/hellomaven/AppTest.java
+++ src/test/java/com/awar905/hellomaven/AppTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
+import com.awar905.hellomaven.Psr;
+
 /**
  * Unit test for simple App.
  */
@@ -15,5 +17,16 @@ public class AppTest {
     @Test
     public void shouldAnswerWithTrue() {
         assertTrue(true);
+    }
+
+    /**
+     * Test transformation of integer to Psr.Move with null handling
+     */
+    @Test
+    public void intToPsrMove() {
+      assertTrue(Psr.intToMove(0).isPresent());
+      assertTrue(Psr.intToMove(1).isPresent());
+      assertTrue(Psr.intToMove(2).isPresent());
+      assertTrue(!Psr.intToMove(3).isPresent());
     }
 }
