4db5e2536e13d6aff96ba422a98e4a3f606e15f8
drflamemontgomery <drflamemontgomery@gmail.com>
2026-02-26 14:58:50 +1300

    Add: Generated Maven Project Structure
    Use archetype:generate to create project structure
    Test compile structure using `mvn project`
    TODO add github unit testing

---
+++ hello-maven/src/main/java/com/awar905/hellomaven/App.java
@@ -0,0 +1,10 @@
+package com.awar905.hellomaven;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
